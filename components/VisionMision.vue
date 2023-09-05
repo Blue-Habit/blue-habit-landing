940f1551c34d6365f0191c1aee4e88b276e44b54
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-05 23:15:54 +0700

    create vision mision

--- components/VisionMision.vue
+++ components/VisionMision.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="container vision mx-auto py-9">
+  <div class="vision mx-auto py-9">
     <h1 class="text-primary text-xl lg:px-16 sm:px-5">Our Vision</h1>
-    <div class="grid lg:grid-cols-2 sm:grid-cols-1">
-      <h1 class="text-4xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa?
+    <div class="grid lg:grid-cols-2 sm:grid-cols-1 lg:px-16 sm:px-5">
+      <h1 class="text-5xl pt-4 font-bold">
+        Innovating Habits <br />for a Transformed <br />Future
       </h1>
+      <p class="pt-4">
+        is not just about financial literacy – it's about creating a paradigm
+        shift in how individuals approach their financial well-being. We
+        envision a future where the power of innovation and ideas drives the
+        creation of habits that pave the way for remarkable transformation.
+      </p>
+    </div>
+  </div>
+  <div class="mision mx-auto">
+    <div class="grid lg:grid-cols-2 sm:grid-cols-1 lg:pl-16 sm:px-5">
+      <img src="@/assets/img/img-mision.png" alt="" class="pt-16 w-4/5" />
+      <div class="grid grid-rows-2">
+        <img src="@/assets/img/img-mision2.png" alt="w-4/5" />
+        <div class="pe-8">
+          <h1 class="text-primary text-xl">Our Mision</h1>
+          <h1 class="text-5xl pt-3 font-bold">
+            Empowering <br />
+            Change Through <br />Self-Initiation
+          </h1>
+          <p class="pt-5 pe-8">
+            We believe in the power of self-initiation to drive meaningful
+            change. Take control of your financial journey, cultivate better
+            habits, and shape a prosperous future. Through our resources and
+            support, you have the tools to ignite change within yourself.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
