ce576d73b25b8808ce886b35b2fb42b822645869
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-05 21:48:25 +0700

    add line

--- components/OurFocus.vue
+++ components/OurFocus.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto py-9">
     <h1 class="text-primary text-2xl lg:px-16 sm:px-5">Our Focus</h1>
-    <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-16">
+    <div class="grid lg:grid-cols-5 sm:grid-cols-1">
       <div class="text-center flex flex-col items-center pt-7">
         <img src="@/assets/img/icon-supportive.png" alt="" class="" />
         <p>
@@ -9,12 +9,18 @@
           And Habit Change
         </p>
       </div>
+      <div class="flex flex-col items-center pt-7">
+        <img src="@/assets/img/line.png" alt="" />
+      </div>
       <div class="text-center flex flex-col items-center pt-7">
         <img src="@/assets/img/icon-growth.png" alt="" class="" />
         <p>
           Growth and Skill <br />
           Developments
         </p>
+      </div>
+      <div class="flex flex-col items-center pt-7">
+        <img src="@/assets/img/line.png" alt="" />
       </div>
       <div class="text-center flex flex-col items-center pt-7">
         <img src="@/assets/img/icon-empowering.png" alt="" class="" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
+    <Hero />
     <OurFocus />
   </div>
 </template>
 
 <script>
+import Hero from "@/components/Hero";
 import OurFocus from "../components/OurFocus.vue";
 
-export default { components: { OurFocus } };
+export default {
+  components: {
+    Hero,
+    OurFocus,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
