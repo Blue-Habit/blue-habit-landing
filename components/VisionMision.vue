b4b55719e4619310ee7aa81f695ffbc3d9038aae
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-05 22:25:21 +0700

    feat : create vision mission

---
+++ components/VisionMision.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container vision mx-auto py-9">
+    <h1 class="text-primary text-xl lg:px-16 sm:px-5">Our Vision</h1>
+    <div class="grid lg:grid-cols-2 sm:grid-cols-1">
+      <h1 class="text-4xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa?
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VisionMision",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Hero />
+    <VisionMision />
   </div>
 </template>
 
 <script>
 import Hero from "@/components/Hero";
+import VisionMision from "@/components/VisionMision";
 export default {
   components: {
     Hero,
+    VisionMision,
   },
 };
 </script>
