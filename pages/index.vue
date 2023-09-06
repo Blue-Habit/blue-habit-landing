0175b368951f250aa29021f9965755086699ec35
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-06 21:44:26 +0700

    feat : add ourfocus in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Hero />
+    <OurFocus />
     <VisionMision />
   </div>
 </template>
 
 <script>
 import Hero from "@/components/Hero";
+import OurFocus from "@/components/OurFocus";
 import VisionMision from "@/components/VisionMision";
 export default {
   components: {
     Hero,
+    OurFocus,
     VisionMision,
   },
 };
