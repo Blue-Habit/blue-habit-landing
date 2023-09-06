c25dbf6ab52fea87fa9280e9080ba6c10dbcb9af
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-06 21:16:44 +0700

    feat : add vision and mission

--- components/VisionMision.vue
+++ components/VisionMision.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="vision mx-auto py-9">
-    <h1 class="text-primary text-xl lg:px-16 sm:px-5">Our Vision</h1>
+  <div class="vision mx-auto py-9 text-gray-100">
+    <h1 class="text-primary text-xl lg:px-16 sm:px-5 text-red-400">
+      Our Vision
+    </h1>
     <div class="grid lg:grid-cols-2 sm:grid-cols-1 lg:px-16 sm:px-5">
-      <h1 class="text-5xl pt-4 font-bold">
+      <h1 class="text-5xl pt-4 font-bold text-gray-900">
         Innovating Habits <br />for a Transformed <br />Future
       </h1>
-      <p class="pt-4">
+      <p class="pt-4 text-gray-700">
         is not just about financial literacy – it's about creating a paradigm
         shift in how individuals approach their financial well-being. We
         envision a future where the power of innovation and ideas drives the
@@ -14,17 +16,27 @@
     </div>
   </div>
   <div class="mision mx-auto">
-    <div class="grid lg:grid-cols-2 sm:grid-cols-1 lg:pl-16 sm:px-5">
-      <img src="@/assets/img/img-mision.png" alt="" class="pt-16 w-4/5" />
+    <div class="grid lg:grid-cols-2 sm:grid-cols-1 justify-between">
       <div class="grid grid-rows-2">
-        <img src="@/assets/img/img-mision2.png" alt="w-4/5" />
+        <img
+          src="@/assets/img/img-mision.png"
+          alt=""
+          class="pt-16 w-4/5 pb-16 lg:pl-16 sm:px-5"
+        />
+        <div class="pt-16 flex-column">
+          <img src="@/assets/img/img-mision3.png" alt="" />
+        </div>
+      </div>
+
+      <div class="grid grid-rows-2">
+        <img src="@/assets/img/img-mision2.png" class="w-full" />
         <div class="pe-8">
           <h1 class="text-primary text-xl">Our Mision</h1>
-          <h1 class="text-5xl pt-3 font-bold">
+          <h1 class="text-5xl pt-3 font-bold text-gray-900">
             Empowering <br />
             Change Through <br />Self-Initiation
           </h1>
-          <p class="pt-5 pe-8">
+          <p class="pt-5 pe-8 text-gray-700">
             We believe in the power of self-initiation to drive meaningful
             change. Take control of your financial journey, cultivate better
             habits, and shape a prosperous future. Through our resources and
