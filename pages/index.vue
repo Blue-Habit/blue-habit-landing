c5fc5bef42ae2daa74898705e09016c1a6ae0276
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-08 22:26:40 +0700

    feat : create teams section

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <Hero />
+    <OurFocus />
+    <VisionMision />
+    <Products />
+    <Teams />
   </div>
 </template>
 
 <script>
 import Hero from "@/components/Hero";
+import OurFocus from "@/components/OurFocus";
+import VisionMision from "@/components/VisionMision";
+import Products from "@/components/Products";
+import Teams from "../components/Teams";
 export default {
   components: {
     Hero,
+    OurFocus,
+    VisionMision,
+    Products,
+    Teams,
   },
 };
 </script>
