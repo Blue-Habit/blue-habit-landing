2f6211fca606061601822c3c8635f10b5adb64f5
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-08 21:39:52 +0700

    fix : change bg body card

--- components/CardTeams.vue
+++ components/CardTeams.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-auto">
     <img :src="imageSrc" class="w-full" alt="..." />
-    <div class="p-4">
+    <div class="p-4 bg-white">
       <h5 class="text-gray-700 mb-2">{{ name }}</h5>
       <p class="text-gray-900 mb-2">{{ position }}</p>
       <a :href="linkedinUrl">
