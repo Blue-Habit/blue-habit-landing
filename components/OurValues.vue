b728796440177f81148f7720f14795906fe56151
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-12-21 21:31:56 +0700

    fix : refactoring our values card

--- components/OurValues.vue
+++ components/OurValues.vue
@@ -5,48 +5,38 @@
                 <h1 class="text-primary text-2xl font-bold mb-5">Our Values</h1>
                 <h1 class="text-5xl font-bold mb-5">Nurturing Growth and Excellence</h1>
                 <p class="text-gray-700">We prioritize team development through learning and growth spaces. Our nurturing
-                    culture celebrates
-                    personal and professional growth, while adhering to industry project management standards.</p>
+                    culture celebrates personal and professional growth, while adhering to industry project management
+                    standards.</p>
             </div>
             <div class="flex flex-col gap-7 w-full">
-                <div class="bg-white w-full p-5">
-                    <img :src="growth" alt="Growth Icon" class="w-1/12 mb-3">
-                    <h1 class="text-xl font-bold mb-3">Learning and Growth Space</h1>
-                    <p class="text-gray-700">Our team's growth is paramount. We've established a learning space for
-                        acquiring new skills and
-                        insights, while our growth space focuses on personal development.</p>
-                </div>
-                <div class="bg-white w-full p-5">
-                    <img :src="supportive" alt="Supportive Icon" class="w-1/12 mb-3">
-                    <h1 class="text-xl font-bold mb-3">Supportive Culture</h1>
-                    <p class="text-gray-700">Our culture revolves around support and rewards. We've created an environment
-                        where personal and professional development are valued and encouraged.</p>
-                </div>
-                <div class="bg-white w-full p-5">
-                    <img :src="empowering" alt="Empowering Icon" class="w-1/12 mb-3">
-                    <h1 class="text-xl font-bold mb-3">Industry-Leading Project Management</h1>
-                    <p class="text-gray-700">In our pursuit of excellence, our project management practices stand up to
-                        industry standards.</p>
+                <div v-for="(value, index) in values" :key="index" class="bg-white w-full p-5">
+                    <img :src="value.icon" :alt="`${value.title} Icon`" class="w-1/12 mb-3">
+                    <h1 class="text-xl font-bold mb-3">{{ value.title }}</h1>
+                    <p class="text-gray-700">{{ value.description }}</p>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
+  
 <script>
 import growth from "@/assets/img/icon-growth.png"
 import supportive from "@/assets/img/icon-supportive.png"
 import empowering from "@/assets/img/icon-empowering.png"
+
 export default {
     name: "OurValues",
     data() {
         return {
-            growth: growth,
-            supportive: supportive,
-            empowering: empowering
+            values: [
+                { title: "Learning and Growth Space", icon: growth, description: "Our team's growth is paramount. We've established a learning space for acquiring new skills and insights, while our growth space focuses on personal development." },
+                { title: "Supportive Culture", icon: supportive, description: "Our culture revolves around support and rewards. We've created an environment where personal and professional development are valued and encouraged." },
+                { title: "Industry-Leading Project Management", icon: empowering, description: "In our pursuit of excellence, our project management practices stand up to industry standards." },
+            ]
         };
     }
 };
 </script>
-
+  
 <style lang="scss" scoped></style>
+  
