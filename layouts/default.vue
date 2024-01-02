048e25a80f1136b37ecf388a222214316049e411
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-02 22:07:15 +0700

    refactor : effective code navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <nav
-    class="nav flex flex-wrap items-center justify-between p-4 lg:px-16 sm:px-5 bg-primary"
-  >
+  <nav class="nav flex flex-wrap items-center justify-between p-4 lg:px-16 sm:px-5 bg-primary">
     <div class="flex flex-no-shrink items-center mr-6 py-3">
       <NuxtLink to="/">
         <img src="@/assets/img/Logo.png" alt="Logo" />
@@ -9,48 +7,13 @@
     </div>
 
     <input class="menu-btn hidden" type="checkbox" id="menu-btn" />
-    <label
-      class="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none"
-      for="menu-btn"
-    >
+    <label class="menu-icon block cursor-pointer md:hidden px-2 py-4 relative select-none" for="menu-btn">
       <span class="navicon flex items-center relative"></span>
     </label>
 
     <ul class="menu flex justify-end list-reset m-0 w-full md:w-auto">
-      <li>
-        <a href="#about" class="block md:inline-block px-4 py-3 text-white"
-          >About us</a
-        >
-      </li>
-
-      <li>
-        <a
-          href="#vision"
-          class="block md:inline-block px-4 py-3 no-underline text-white"
-          >Vision</a
-        >
-      </li>
-
-      <li>
-        <a
-          href="#mission"
-          class="block md:inline-block px-4 py-3 no-underline text-white"
-          >Mission</a
-        >
-      </li>
-      <li>
-        <a
-          href="#product"
-          class="block md:inline-block px-4 py-3 no-underline text-white"
-          >Product</a
-        >
-      </li>
-      <li>
-        <a
-          href="#team"
-          class="block md:inline-block px-4 py-3 no-underline text-white"
-          >Team</a
-        >
+      <li v-for="item in menuItems" :key="item.id">
+        <a :href="item.href" class="block md:inline-block px-4 py-3 no-underline text-white">{{ item.label }}</a>
       </li>
     </ul>
   </nav>
@@ -59,7 +22,15 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const menuItems = ref([
+  { id: 1, label: 'About us', href: '#about' },
+  { id: 2, label: 'Vision', href: '#vision' },
+  { id: 3, label: 'Mission', href: '#mission' },
+  { id: 4, label: 'Product', href: '#product' },
+  { id: 5, label: 'Team', href: '#team' },
+]);
+</script>
 
 <style>
 @media (max-width: 767px) {
@@ -87,28 +58,28 @@
     top: -5px;
   }
 
-  .menu-btn:not(:checked) ~ .menu {
+  .menu-btn:not(:checked)~.menu {
     display: none;
   }
 
-  .menu-btn:checked ~ .menu {
+  .menu-btn:checked~.menu {
     display: block;
   }
 
-  .menu-btn:checked ~ .menu-icon .navicon {
+  .menu-btn:checked~.menu-icon .navicon {
     background: transparent;
   }
 
-  .menu-btn:checked ~ .menu-icon .navicon::before {
+  .menu-btn:checked~.menu-icon .navicon::before {
     transform: rotate(-45deg);
   }
 
-  .menu-btn:checked ~ .menu-icon .navicon::after {
+  .menu-btn:checked~.menu-icon .navicon::after {
     transform: rotate(45deg);
   }
 
-  .menu-btn:checked ~ .menu-icon .navicon::before,
-  .menu-btn:checked ~ .menu-icon .navicon::after {
+  .menu-btn:checked~.menu-icon .navicon::before,
+  .menu-btn:checked~.menu-icon .navicon::after {
     top: 0;
   }
 }
